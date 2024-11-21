ac85dca9639bebb70a86fef3f0840d53957ad444
GeorgeGarnett-O <gg403@kent.ac.uk>
2024-11-21 14:40:09 +0000

    manager

---
+++ student_manager.py
@@ -0,0 +1,19 @@
+add_student(name,age,grade):
+ 
+open (student.text) as file: 
+
+display_student(name,age,grade):
+
+with open("student.text") as file:
+    
+    file.write(f"{display}\n")
+
+
+find_student(name):
+
+with open("student.text") as file:
+    file.write(f"{name},{age},{grade}\n")
+
+
+return student.text: 
+
